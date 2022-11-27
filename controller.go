f3522135d728a04d8e68f948a6652aa24462f845
Jackson Winslow <securablesneakers@gmail.com>
2022-11-27 14:39:28 -0500

    base init code complete

--- controller.go
+++ controller.go
@@ -1,9 +1,93 @@
 package main
 
 import (
+	"bufio"
+	"encoding/gob"
 	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
 )
 
+func handle_connections(source net.Listener, nodes *sync.Map) {
+	for {
+		c, err := source.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Handle incoming messages from client
+		go func() {
+			dec := gob.NewDecoder(c)
+			var message string
+			nodeport := ""
+			for {
+				err = dec.Decode(&message)
+				if err != nil {
+					return
+				}
+
+				// Add node encoder to nodes map on receiving first message, assuming it is the port from node
+				if nodeport == "" {
+					(*nodes).Store(message, gob.NewEncoder(c))
+					nodeport = message
+					fmt.Println("New Node: " + nodeport)
+					continue
+				}
+
+				// Handle other messages from node
+			}
+		}()
+	}
+}
+
 func main() {
-	
+
+	// Initialize receiver
+	address := "127.0.0.1:8080" //+ port
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer l.Close()
+
+	var nodes sync.Map
+
+	go handle_connections(l, &nodes)
+
+	// Awaits user input to begin experiment
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+
+		text = strings.TrimSpace(text)
+
+		faults, err := strconv.Atoi(text)
+		if err != nil {
+			fmt.Println("Please enter an integer for fault tolerance")
+			continue
+		}
+
+		var nodeList []string
+
+		// Create nodeList
+		nodes.Range(func(key, value interface{}) bool {
+			nodeList = append(nodeList, key.(string))
+			return true
+		})
+
+		nodes.Range(func(key, value interface{}) bool {
+			outgoingEnc := value.(*gob.Encoder)
+			messageMap := make(map[string]interface{})
+			messageMap["faults"] = faults
+			messageMap["nodes"] = nodeList
+			outgoingEnc.Encode(messageMap)
+			return true
+		})
+	}
 }
--- node.go
+++ node.go
@@ -1,5 +1,70 @@
 package main
 
+import (
+	"encoding/gob"
+	"fmt"
+	"net"
+	"os"
+)
+
 func main() {
 
+	var n, f int
+	var nodes []string
+
+	port := os.Args[1]
+	// state := os.Args[2]
+
+	// Setup controller connection
+	c, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Sends the controller port # upon connection initialization
+	enc := gob.NewEncoder(c)
+	enc.Encode(port)
+
+	// Handles init message from controller
+	dec := gob.NewDecoder(c)
+	var message map[string]interface{}
+	decerr := dec.Decode(&message)
+	if decerr == nil {
+		nodes = message["nodes"].([]string)
+		n = len(nodes)
+		f = message["faults"].(int)
+		fmt.Println(nodes, n, f)
+	}
+
+	// round := 0
+	// for {
+	// 	// UNICAST TO EVERYONE ELSE INCLUDING SELF
+
+	// 	// WAIT UNTIL RECEIVED N-F messages including self
+	// 	avg := 0
+	// 	count := 0
+
+	// 	// Wait for n - f messages received
+
+	// 	// Calculate average
+	// 	for count < (n - f) {
+	// 		avg = avg + // received value
+	// 	}
+
+	// 	avg = avg / (n - f)
+
+	// 	state = avg
+
+	// 	round = round + 1
+
+	// 	// CHECK IF WE SHOULD KEEP GOING
+	// 	if false {
+
+	// 	} else {
+	// 		break;
+	// 	}
+	// }
+
+	// fmt.Println("Reached consenus")
 }
